00f59acfb510c8c52e96e5e3987aa83cad2576ba
binhk <binhkhuu90@gmail.com>
2023-08-12 16:31:40 +0800

    Inistial setup for Api project. Install EF. Run migrations.

---
+++ ApplicationInsightsLogging.Core/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInsightsLogging.Core.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Supplier is Required")]
+        public string Supplier { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "Price should be at least one dollar")]
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ApplicationInsightsLogging/DbContext/ApplicationInsightsLoggerContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationInsightsLogging.Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInsightsLogging.Infrastructure.DbContext
+{
+    public class ApplicationInsightsLoggerContextFactory : IDesignTimeDbContextFactory<ApplicationInsightsLoggerContext>
+    {
+        public ApplicationInsightsLoggerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationInsightsLoggerContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ApplicationInsightsLogger;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            return new ApplicationInsightsLoggerContext(optionsBuilder.Options);
+        }
+    }
+}
