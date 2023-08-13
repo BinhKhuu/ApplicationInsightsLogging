eff8b5e076a71a5bf0a86b0aefc4a39ee003b4f6
binhk <binhkhuu90@gmail.com>
2023-08-13 15:28:36 +0800

    move api project in to correct folder. Load settings from local settings or environment.

--- LoggingFunctions/Endpoints/Trace.cs
+++ LoggingFunctions/Endpoints/Trace.cs
@@ -9,25 +9,48 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ApplicationInsightsLogging.Infrastructure.DBContext;
 using System.Linq;
+using Microsoft.ApplicationInsights.Extensibility;
+using ApplicationInsightsLogging.Api.Services;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using ApplicationInsightsLogging.Core.Models;
+using System.Collections.Generic;
 
 namespace ApplicationInsightsLogging.Api.Endpoints
 {
     public class Trace
     {
         private ApplicationInsightsLoggerContext _loggerContext;
-        public Trace(ApplicationInsightsLoggerContext loggerContext)
+        private TelementryService _telementryService;
+        public Trace(ApplicationInsightsLoggerContext loggerContext, TelementryService telementryService)
         {
             _loggerContext = loggerContext;
+            _telementryService = telementryService;
         }
 
-        [FunctionName("Trace")]
+        [FunctionName("TraceProps")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             var item = _loggerContext.Products.First();
+
+            var client = _telementryService.Client;
+            // prop__ set directly
+            var traceTelementry = new TraceTelemetry();
+            traceTelementry.Properties["RequestDate"] = DateTime.Today.ToString("yyyy-MM-dd hh:mm:ss");
+            traceTelementry.Message = "Request Date";
+
+            // prop__ set through Dictionary<string,string>
+            client.TrackTrace("First Item Name", SeverityLevel.Information, new Dictionary<string, string> { { "FirstItemName", item.Name } });
+            client.TrackTrace(traceTelementry);
+    
+            // prop__ set through scope
+            using(log.BeginScope(new Dictionary<string, object> { ["FirstItem"] = item }))
+            {
+                // prop__ set through template only works with ILogger
+                log.LogInformation("First Item is {FirstItem}", JsonConvert.SerializeObject(item));
+            }
 
 
             return new OkObjectResult("Ugh");
---
+++ LoggingFunctions/Services/TelementryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace ApplicationInsightsLogging.Api.Services
+{
+    public class TelementryService
+    {
+        public TelemetryClient Client { get; set; }
+        public TelementryService(string connectionString) {
+            var telementryConfig = TelemetryConfiguration.CreateDefault();
+            telementryConfig.ConnectionString = connectionString;
+            Client = new TelemetryClient(telementryConfig); 
+        }
+    }
+}
