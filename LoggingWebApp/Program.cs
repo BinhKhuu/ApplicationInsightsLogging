5c714d552260cc135a57172c85cb00533ab3cafd
binhk <binhkhuu90@gmail.com>
2023-08-19 16:48:36 +0800

    add application insights logging blazor package but not working.

--- LoggingWebApp/Program.cs
+++ LoggingWebApp/Program.cs
@@ -1,3 +1,4 @@
+using BlazorApplicationInsights;
 using LoggingWebApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -13,5 +14,18 @@ builder.Services.AddMsalAuthentication(options =>
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
 
+builder.Services.AddBlazorApplicationInsights(async applicationInsights =>
+{
+	var telemetryItem = new TelemetryItem()
+	{
+		Tags = new Dictionary<string, object>()
+		{
+			{ "ai.cloud.role", "SPA" },
+			{ "ai.cloud.roleInstance", "Blazor Wasm" },
+		}
+	};
+
+	await applicationInsights.AddTelemetryInitializer(telemetryItem);
+});
 
 await builder.Build().RunAsync();
